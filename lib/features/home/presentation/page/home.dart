057715263dd0a84ff6cd728cdca50907e3a526c0
azhari-zaidie <azharizaidie.developer@gmail.com>
2025-03-16 16:06:44 +0800

    feat: home page

--- lib/features/home/presentation/page/home.dart
+++ lib/features/home/presentation/page/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hendshake_test_flutter/core/route/route.dart';
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
@@ -8,8 +9,32 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Home'),
       ),
-      body: const Center(
-        child: Text('Home'),
+      body: Center(
+        child: Column(
+          children: [
+            // Button to navigate to the activity page
+            ElevatedButton(
+              onPressed: () => Navigator.pushNamed(context, AppRoute.activity),
+              child: const Text('Activity'),
+            ),
+
+            // Dropdown to select the activity type
+            // DropdownButton<String>(
+            //   value: selectedActivityType,
+            //   onChanged: (String? newValue) {
+            //     setState(() {
+            //       selectedActivityType = newValue;
+            //     });
+            //   },
+            //   items: activityTypes.map((String value) {
+            //     return DropdownMenuItem<String>(
+            //       value: value,
+            //       child: Text(value),
+            //     );
+            //   }).toList(),
+            // ),
+          ],
+        ),
       ),
     );
   }
